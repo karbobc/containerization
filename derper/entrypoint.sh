59549bb65b01d8f21d0bdcfa576bce6fa5a95e4b
Karbob <karbobc@gmail.com>
2024-05-14 15:37:37 +0800

    fix(derper): fix derper argument order

--- derper/entrypoint.sh
+++ derper/entrypoint.sh
@@ -4,12 +4,12 @@ set -eu
 
 # https://tailscale.com/kb/1118/custom-derp-servers
 exec /app/derper -hostname "${TS_DERP_HOSTNAME}" \
-  -certdir "${TS_DERP_CERTDIR}" \
-  -certmode "${TS_DERP_CERTMODE}" \
-  -verify-clients "${TS_DERP_VERIFY_CLIENTS}" \
   -a "${TS_DERP_ADDR}" \
   -http-port "${TS_DERP_HTTP_PORT}" \
   -stun-port "${TS_DERP_STUN_PORT}" \
+  -certdir "${TS_DERP_CERTDIR}" \
+  -certmode "${TS_DERP_CERTMODE}" \
+  -verify-clients "${TS_DERP_VERIFY_CLIENTS}" \
   -stun \
   -derp \
   "$@"
