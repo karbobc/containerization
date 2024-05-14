2b21bc26729e3829863201860b73c6053e053bfb
Karbob <karbobc@gmail.com>
2024-05-14 14:53:55 +0800

    feat(derper): add more env support
    1. `TS_DERP_ADDR` env support.
    2. `TS_DERP_HTTP_PORT` env support.
    3. `TS_DERP_STUN_PORT` env support.

--- derper/entrypoint.sh
+++ derper/entrypoint.sh
@@ -7,6 +7,9 @@ exec /app/derper -hostname "${TS_DERP_HOSTNAME}" \
   -certdir "${TS_DERP_CERTDIR}" \
   -certmode "${TS_DERP_CERTMODE}" \
   -verify-clients "${TS_DERP_VERIFY_CLIENTS}" \
-  -derp \
+  -a "${TS_DERP_ADDR}" \
+  -http-port "${TS_DERP_HTTP_PORT}" \
+  -stun-port "${TS_DERP_STUN_PORT}" \
   -stun \
+  -derp \
   "$@"
