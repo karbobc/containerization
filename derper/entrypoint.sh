6dd78b97e66e504efeb65fcb049501ffc560bcba
Karbob <karbobc@gmail.com>
2024-05-14 10:20:10 +0800

    feat(derper): add `TS_DERP_VERIFY_CLIENTS` env support

--- derper/entrypoint.sh
+++ derper/entrypoint.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env sh
 
-if [ -z "${TS_DERP_HOSTNAME}" ]; then
-    echo "'TS_DERP_HOSTNAME' environment variable must be set!!!"
-    exit 1
-fi
+set -eu
 
 # https://tailscale.com/kb/1118/custom-derp-servers
 exec /app/derper -hostname "${TS_DERP_HOSTNAME}" \
   -certdir "${TS_DERP_CERTDIR}" \
   -certmode "${TS_DERP_CERTMODE}" \
-  -verify-clients \
+  -verify-clients "${TS_DERP_VERIFY_CLIENTS}" \
   -derp \
   -stun \
   "$@"
